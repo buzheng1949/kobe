a654af1e0ba48a24c1d19353298d0541d87b922d
buzheng <buzheng@meili-inc.com>
2017-08-15 01:12:48 +0800

    feat:完善自定义分页的回传参数

--- src/main/java/com/tuan/pagehelper/AdvertisementParams.java
+++ src/main/java/com/tuan/pagehelper/AdvertisementParams.java
@@ -3,9 +3,12 @@ package com.tuan.pagehelper;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.tuan.utils.JSONUtils;
+import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.lang3.math.NumberUtils;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.map.util.JSONPObject;
+
+import java.io.UnsupportedEncodingException;
 
 /**
  * Created by buzheng on 17/8/13.
@@ -20,7 +23,13 @@ public class AdvertisementParams extends BaseParams {
         if (pageSize != null) {
             obj.addProperty("pageSize", pageSize);
         }
-        return obj.toString();
+        byte[] encodeBase64Mbook = null;
+        try {
+            encodeBase64Mbook = Base64.encodeBase64(obj.toString().getBytes("UTF-8"));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return new String(encodeBase64Mbook);
     }
 
     public int getPage() {
@@ -31,6 +40,18 @@ public class AdvertisementParams extends BaseParams {
         return NumberUtils.toInt(parseMbook("pageSize"), 10);
     }
 
+    private int page;
+
+    private int pageSize;
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
+    }
+
     /**
      * 字符串转目标对象
      *
@@ -38,7 +59,14 @@ public class AdvertisementParams extends BaseParams {
      * @return
      */
     public static AdvertisementParams covertToAdvertisementParams(String message) {
+        byte[] asBytes = Base64.decodeBase64(message);
+        try {
+            message = new String(asBytes, "utf-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
         AdvertisementParams advertisementParams = new AdvertisementParams();
+        advertisementParams.setmBook(message);
         Object object = JSONUtils.JSONToObj(message, AdvertisementParams.class);
         if (object instanceof AdvertisementParams) {
             advertisementParams = (AdvertisementParams) object;
